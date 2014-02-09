d549a9fbdd02efbf2cd93591264a615717c397ce
Benjamin van der Veen <b@bvanderveen.com>
2014-02-08 17:19:29 -0800

    create user according to env

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 set -x
 
-ssh root@$HOST "adduser --disabled-password --ingroup sudo --shell /bin/bash --gecos \"\" bvanderveen; mkdir /home/$USER/.ssh; echo \"%sudo ALL=NOPASSWD: ALL\" > /etc/sudoers.d/nopasswd"
+ssh root@$HOST "adduser --disabled-password --ingroup sudo --shell /bin/bash --gecos \"\" $USER; mkdir /home/$USER/.ssh; echo \"%sudo ALL=NOPASSWD: ALL\" > /etc/sudoers.d/nopasswd"
 scp ~/.ssh/id_rsa.pub root@$HOST:/home/$USER/.ssh/authorized_keys
 
 scp provision_box.sh $HOST:/home/$USER/provision_box.sh
